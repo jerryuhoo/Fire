ae6476777f3dd2264eb67f0fd6abdc380b9385f3
Jerry Yu <Jerryuhoo@gmail.com>
2020-02-27 17:10:10 -0600

    upgrade to version 0.4
    1. upgrade GUI
    2. fix a bug of graph when it is downsample mode.

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -9,7 +9,7 @@
 namespace BinaryData
 {
     extern const char*   blood_background_png;
-    const int            blood_background_pngSize = 30166;
+    const int            blood_background_pngSize = 148658;
 
     extern const char*   blood_png;
     const int            blood_pngSize = 20679;

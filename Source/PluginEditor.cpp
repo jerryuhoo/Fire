a34e9de027f525df524fd7133874677328ea08b0
jerryuhoo <yyflongjuan@126.com>
2020-06-30 16:09:43 +0800

    upgrade to 0.56
    UI changed. (Toggle button)

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -10,7 +10,7 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
-#define VERSION "0.55"
+#define VERSION "0.56"
 //==============================================================================
 BloodAudioProcessorEditor::BloodAudioProcessorEditor(BloodAudioProcessor &p)
     : AudioProcessorEditor(&p), processor(p)

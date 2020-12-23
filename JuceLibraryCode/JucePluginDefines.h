0ae25e34479ec71bc667318f0e569e7819b0def6
jerryuhoo <Jerryuhoo@gmail.com>
2020-12-23 21:33:55 +0800

    0.791 multiband 9 (init multiband fixed)
    Menu - init
    Fixed: click init will reset all GUI components

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -74,13 +74,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                0.790
+ #define JucePlugin_Version                0.791
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x31600
+ #define JucePlugin_VersionCode            0x31700
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "0.790"
+ #define JucePlugin_VersionString          "0.791"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode

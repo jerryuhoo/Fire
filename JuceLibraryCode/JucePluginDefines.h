9c67503cd6efd497138a74ad6de2301dbf0e0624
jerryuhoo <yyflongjuan@126.com>
2021-01-18 09:58:36 +0800

    0.82.4 Fixed multiband: moving the line doesn't change the frequency
    Former-commit-id: 9f10f5730c93cfbf14f8ef644749da22be04dad9

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -74,13 +74,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                0.82.3
+ #define JucePlugin_Version                0.82.4
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x5203
+ #define JucePlugin_VersionCode            0x5204
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "0.82.3"
+ #define JucePlugin_VersionString          "0.82.4"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode

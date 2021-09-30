3ad03bb0d826c24bb13d66e5fb1d76fd1587c009
jerryuhoo <Jerryuhoo@gmail.com>
2021-10-01 07:57:35 +0800

    0.9.3 b23 cleanup 7 (Focus bug fixed!)
    1. Focus bug fixed
    2. Fixed windows popup menu transparent issue

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -47,7 +47,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Fire";
     const char* const  companyName    = "";
-    const char* const  versionString  = "0.9.3 b21";
+    const char* const  versionString  = "0.9.3 b23";
     const int          versionNumber  = 0x903;
 }
 #endif

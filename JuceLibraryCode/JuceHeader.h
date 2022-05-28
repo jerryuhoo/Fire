283d4b9ae830de213c6104e9b6c521bb4d9d25b9
jerryuhoo <Jerryuhoo@gmail.com>
2022-05-28 11:12:18 +0800

    bump up version to 0.9.6

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -47,7 +47,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Fire";
     const char* const  companyName    = "";
-    const char* const  versionString  = "0.9.5.1";
-    const int          versionNumber  = 0x90501;
+    const char* const  versionString  = "0.9.6";
+    const int          versionNumber  = 0x906;
 }
 #endif

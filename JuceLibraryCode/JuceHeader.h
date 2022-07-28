011e63403dee83256f032a264844f1d78e0cf530
jerryuhoo <Jerryuhoo@gmail.com>
2022-07-28 14:44:06 +0800

    bump up version to v1.0.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -47,7 +47,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Fire";
     const char* const  companyName    = "";
-    const char* const  versionString  = "1.0.0.1";
-    const int          versionNumber  = 0x1000001;
+    const char* const  versionString  = "1.0.0";
+    const int          versionNumber  = 0x10000;
 }
 #endif
